3f8341d9b475e6fbdc369eb4f216df8d8c43dc8b
ChangeMPU <shatovoleg@gmail.com>
2022-03-12 19:52:38 +0500

    Add login screen

---
+++ EZ Bank/EZ Bank/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  EZ Bank
+//
+//  Created by Oleg Shatov on 12.03.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = LoginViewController()
+
+        return true
+    }
+}
+
